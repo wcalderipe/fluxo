af05feef642cd0d64bf04a9f7bf5aa2739b6571e
William Calderipe <wcalderipe@gmail.com>
2020-09-28 20:49:00 +0200

    Initial commit only with files from re-frame-template
    ```
    lein new re-frame fluxo +cider +test
    ```

---
+++ src/cljs/fluxo/views.cljs
@@ -0,0 +1,11 @@
+(ns fluxo.views
+  (:require
+   [re-frame.core :as re-frame]
+   [fluxo.subs :as subs]
+   ))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1 "Hello from " @name]
+     ]))
