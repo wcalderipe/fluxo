3babf3a544e0c6eca3368b64eb5c82d408094e33
William Calderipe <wcalderipe@gmail.com>
2020-10-16 08:59:33 +0200

    Add with-stream database utility fn
    Add a function to fake stream data in database for development purposes.

--- src/cljs/fluxo/db.cljs
+++ src/cljs/fluxo/db.cljs
@@ -1,15 +1,29 @@
 (ns fluxo.db
-  (:require [re-frame.core :refer [reg-event-db reg-sub]]
-            [fluxo.wallet :refer [->Asset]]))
+  (:require [re-frame.core :refer [reg-event-db reg-sub]]))
 
-(def ^:const ropsten-testdai (->Asset "TestDAI"
-                                      "DAI"
-                                      "0x2d69ad895797c880abce92437788047ba0eb7ff6"))
+(def ^:const ropsten-testdai {:name    "TestDAI"
+                              :symbol  "DAI"
+                              :address "0x2d69ad895797c880abce92437788047ba0eb7ff6"})
 
 (def default-db {:routes {:active :home}
 
                  :wallet {:address nil
                           :assets [ropsten-testdai]}})
+
+(defn- with-stream
+  "Utility function to initialize the database with a valid stream for development
+  purpose."
+  [db]
+  (assoc db :stream {:id              "140"
+                     :deposit-amount  "499999999999999986000"
+                     :rate-per-second "27777777777777777"
+                     :sender-addr     "0xfOO1111111111111111111111111111111111bAr"
+                     :recipient-addr  "0xbAR2222222222222222222222222222222222BaZ"
+                     :start-time      1602827222
+                     :stop-time       1602845222
+                     :token           {:name    "TestDAI"
+                                       :symbol  "DAI"
+                                       :address "0x2d69ad895797c880abce92437788047ba0eb7ff6"}}))
 
 (reg-event-db
  :db/initialize
